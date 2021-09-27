04401e052e27fe3ae2164c13bc669d0c3382f6e6
João Guilherme <alvesjg@gmail.com>
2021-09-27 11:12:09 -0300

    funcao quit

--- simplemail.sh
+++ simplemail.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-
-echo simplemail\>
+#function createuser{
+#	mkdir ./users/$1
+#	touch pass.txt 
+#	cat $2 > pass.txt
+#}
+while [ 1 ]; do
+		echo -n simplemail\>\ 
+		read COMM
+		if [ $COMM = "quit" ]; then
+			exit
+		fi
+done
