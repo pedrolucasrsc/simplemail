564d8fcd022cbf6fe13706665eb15531b3e47443
plucas <pedrosicampos08@gmail.com>
2021-09-28 20:04:35 -0300

    Enter e clear complete

--- mac216-simplemail.sh
+++ mac216-simplemail.sh
@@ -4,15 +4,15 @@ function createuser {
 	if [ -d ./simplemail/users/$1 ]; then
 		echo "Error: This user already exists"
 	else
-    mkdir -p ./simplemail/users/$1/msg 
+        mkdir -p ./simplemail/users/$1/msg 
 		echo $2 | cat > ./simplemail/users/$1/pass.txt
 	fi
 }
 
 function login {
 	if [ -d ./simplemail/users/$1 ]; then
-  	if [ "$2" = "$(cat ./simplemail/users/$1/pass.txt)" ] ; then
-			LOGIN=$1
+  	    if [ "$2" = "$(cat ./simplemail/users/$1/pass.txt)" ] ; then
+            LOGIN=$1
 			echo $LOGIN
 		else
 			echo "Error: Password incorrect" 
@@ -29,19 +29,21 @@ function passwd {
     	echo "Error: Username or Password incorrect"
     fi
 }
+
 function listuser {
-		for user in $( ls ./simplemail/users ); do
-			echo $user
-		done
+	for user in $( ls ./simplemail/users ); do
+		echo $user
+	done
 }
+
 function msg {
 	if [ ! "$LOGIN" = " " ];then
-			if [ -d ./simplemail/users/$1 ];then
-			  echo "What's yout message?'CTRL-D' to stop"
-				cat > ./simplemail/users/$1//msg/"| N | $( date ) | $LOGIN"
-			else 
-				echo "This user doesn't exist"
-			fi
+		if [ -d ./simplemail/users/$1 ];then
+		    echo "What's yout message?'CTRL-D' to stop"
+			cat > ./simplemail/users/$1//msg/"| N | $( date ) | $LOGIN"
+		else 
+			echo "This user doesn't exist"
+		fi
 	else
 		echo "Please log into your account"
 	fi
@@ -57,14 +59,18 @@ LOGIN=" "
 while [ true ]; do
 		echo -n simplemail\>\ 
 		read -a COMM
-		if [ ${COMM[0]} = "quit" ]; then
+        if [ ${#COMM} = 0 ]; then
+            :
+        elif [ ${COMM[0]} = "clear" ]; then
+            clear
+		elif [ ${COMM[0]} = "quit" ]; then
 			exit
 		elif [ ${COMM[0]} = "createuser" ]; then
 			createuser ${COMM[1]} ${COMM[2]}
 		elif [ ${COMM[0]} = "login" ]; then
 			login ${COMM[1]} ${COMM[2]}
-    elif [ ${COMM[0]} = "passwd" ]; then
-      passwd ${COMM[1]} ${COMM[2]} ${COMM[3]}
+        elif [ ${COMM[0]} = "passwd" ]; then
+            passwd ${COMM[1]} ${COMM[2]} ${COMM[3]}
 		elif [ ${COMM[0]} = "listusers" ]; then
 			listuser
 		elif [ ${COMM[0]} = "msg" ]; then
