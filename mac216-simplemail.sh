f2b9c7c5b0e36fde796e160d0189146b42824281
João Guilherme <alvesjg@gmail.com>
2021-09-28 19:47:01 -0300

    listusers , msg complete

--- mac216-simplemail.sh
+++ mac216-simplemail.sh
@@ -4,14 +4,14 @@ function createuser {
 	if [ -d ./simplemail/users/$1 ]; then
 		echo "Error: This user already exists"
 	else
-        mkdir ./simplemail/users/$1 
+    mkdir -p ./simplemail/users/$1/msg 
 		echo $2 | cat > ./simplemail/users/$1/pass.txt
 	fi
 }
 
 function login {
 	if [ -d ./simplemail/users/$1 ]; then
-        if [ "$2" = "$(cat ./simplemail/users/$1/pass.txt)" ] ; then
+  	if [ "$2" = "$(cat ./simplemail/users/$1/pass.txt)" ] ; then
 			LOGIN=$1
 			echo $LOGIN
 		else
@@ -24,18 +24,36 @@ function login {
 
 function passwd {
     if [ -d ./simplemail/users/$1 -a "$2" = "$(cat ./simplemail/users/$1/pass.txt)" ]; then
-        echo $3 | cat > ./simplemail/users/$1/pass.txt
+    	echo $3 | cat > ./simplemail/users/$1/pass.txt
     else
-        echo "Error: Username or Password incorrect"
+    	echo "Error: Username or Password incorrect"
     fi
+}
+function listuser {
+		for user in $( ls ./simplemail/users ); do
+			echo $user
+		done
+}
+function msg {
+	if [ ! "$LOGIN" = " " ];then
+			if [ -d ./simplemail/users/$1 ];then
+			  echo "What's yout message?'CTRL-D' to stop"
+				cat > ./simplemail/users/$1//msg/"| N | $( date ) | $LOGIN"
+			else 
+				echo "This user doesn't exist"
+			fi
+	else
+		echo "Please log into your account"
+	fi
 }
 
 if [ ! -d ./simplemail ]; then
     mkdir ./simplemail
 fi
 if [ ! -d ./simplemail/users ]; then
-    mkdir ./simplemail/users
+    mkdir -p ./simplemail/users
 fi
+LOGIN=" "
 while [ true ]; do
 		echo -n simplemail\>\ 
 		read -a COMM
@@ -45,7 +63,11 @@ while [ true ]; do
 			createuser ${COMM[1]} ${COMM[2]}
 		elif [ ${COMM[0]} = "login" ]; then
 			login ${COMM[1]} ${COMM[2]}
-        elif [ ${COMM[0]} = "passwd" ]; then
-            passwd ${COMM[1]} ${COMM[2]} ${COMM[3]}
+    elif [ ${COMM[0]} = "passwd" ]; then
+      passwd ${COMM[1]} ${COMM[2]} ${COMM[3]}
+		elif [ ${COMM[0]} = "listusers" ]; then
+			listuser
+		elif [ ${COMM[0]} = "msg" ]; then
+			msg ${COMM[1]}
 		fi
 done
